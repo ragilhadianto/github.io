bb1f80f2c0c50ea083ed490f4a8b867dda7e5334
rgl11 <tripleone92@gmail.com>
2020-07-30 20:49:20 +0700

    20200730 b

--- carstensz/assets/js/custom.js
+++ carstensz/assets/js/custom.js
@@ -3,10 +3,10 @@ $(window).scroll(function() {
     var scrollpos = $(window).scrollTop();
     if (scrollpos > $(".header").outerHeight()) {
         $(".header").removeClass("header-bg-transparent");
-        $(".header__home .logo").attr("src","../assets/img/logo.png");
+        $(".header__home .logo").attr("src","assets/img/logo.png");
     } else {
         $(".header").addClass("header-bg-transparent");
-        $(".header__home .logo").attr("src","../assets/img/logo_white.png");
+        $(".header__home .logo").attr("src","assets/img/logo_white.png");
     }
 });
 
