f2699169b0827fe67af91b599321aa3230ddc383
rgl11 <tripleone92@gmail.com>
2020-09-04 02:08:50 +0700

    20200904 b

--- indonesiakaya/js/main.js
+++ indonesiakaya/js/main.js
@@ -24,6 +24,18 @@ $(document).ready(function(){
         $(".section__item__inner2").show();
     });
 
+
+    //header scroll
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".header").style.top = "0";
+        } else {
+            document.querySelector(".header").style.top = "-83px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
    
 });
 
