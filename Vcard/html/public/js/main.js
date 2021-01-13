fd436ee20c45892f36d27d50bc3ebe15dce195c1
infostaging <infobarismail@gmail.com>
2021-01-13 15:24:21 +0700

    20210113 siang

--- Vcard/html/public/js/main.js
+++ Vcard/html/public/js/main.js
@@ -7,15 +7,46 @@ $(document).ready(function () {
     if ($(this).attr("data-click-state") == 1) {
       $(this).attr("data-click-state", 0);
       $(iconMenu).attr("src", "images/svg/cancel.svg");
+      $("body").css({
+        height: "100%",
+        overflow: "hidden",
+      });
       $(".menu").addClass("right-0");
       $(".menu").removeClass("-right-full");
       //console.log("0");
     } else {
       $(this).attr("data-click-state", 1);
       $(iconMenu).attr("src", "images/svg/menu.svg");
+      $("body").css({
+        height: "auto",
+        overflow: "unset",
+      });
       $(".menu").removeClass("right-0");
       $(".menu").addClass("-right-full");
       //console.log("1");
     }
+  });
+
+  //menu
+  var headerMenu = $("#menu-header strong");
+  $(".menu-inner li a").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr("data-related");
+    var thisClick = $(this);
+    $(".menu .menu-inner").each(function () {
+      if ($(this).attr("id") == id) {
+        $(thisClick).parent().parent(".menu-inner").addClass("-left-full");
+        $(this).addClass("-left-2/4	");
+        $("#menu-header > img").show();
+        $(headerMenu).text("BACK");
+      }
+    });
+  });
+  $("#menu-header").on("click", function (e) {
+    e.preventDefault();
+    $(".menu-inner").removeClass("-left-full");
+    $(".menu-inner").removeClass("-left-2/4	");
+    $(headerMenu).text("MENU");
+    $("#menu-header > img").hide();
   });
 });
