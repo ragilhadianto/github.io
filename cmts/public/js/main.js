3b20f4414765c6a6c46f976af4725ece49a8fef8
ragilhadianto <tripleone92@gmail.com>
2021-03-13 00:37:02 +0700

    20210313

--- cmts/public/js/main.js
+++ cmts/public/js/main.js
@@ -34,3 +34,22 @@ $(".slider").slick({
     },
   ],
 });
+
+//backtotop
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  console.log(height);
+  if (height > 200) {
+    $(".backtotop").fadeIn();
+  } else {
+    $(".backtotop").fadeOut();
+  }
+});
+$(document).ready(function () {
+  $(".backtotop").fadeOut();
+  $(".backtotop").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+});
