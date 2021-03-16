014f975a7c4e0de8cfdb1cb66f07b433e6f1c880
ragilhadianto <tripleone92@gmail.com>
2021-03-17 00:29:53 +0700

    upadte logo cmts

---
+++ leboye/public/js/main.js
@@ -0,0 +1,45 @@
+//menu
+$(".menu__burger").on("click", function () {
+  if ($(this).attr("data-click-state") == 1) {
+    $(this).attr("data-click-state", 0);
+    $(this).children().attr("src", "images/svg/menu__close.svg");
+    $(".menu__inner").fadeIn();
+    console.log("0");
+  } else {
+    $(this).attr("data-click-state", 1);
+    $(this).children().attr("src", "images/svg/menu.svg");
+    $(".menu__inner").fadeOut();
+    console.log("1");
+  }
+});
+
+//slider
+$(".slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  appendDots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+// //backtotop
+// $(window).scroll(function () {
+//   var height = $(window).scrollTop();
+//   console.log(height);
+//   if (height > 200) {
+//     $(".backtotop").fadeIn();
+//   } else {
+//     $(".backtotop").fadeOut();
+//   }
+// });
+// $(document).ready(function () {
+//   $(".backtotop").fadeOut();
+//   $(".backtotop").click(function (event) {
+//     event.preventDefault();
+//     $("html, body").animate({ scrollTop: 0 }, "slow");
+//     return false;
+//   });
+// });
