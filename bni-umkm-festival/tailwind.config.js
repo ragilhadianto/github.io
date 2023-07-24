af0e1025906a4356d0a7415f17e5006f6457199c
ragilhadianto <tripleone92@gmail.com>
2023-07-25 05:37:26 +0700

    bni umkm 25

--- bni-umkm-festival/tailwind.config.js
+++ bni-umkm-festival/tailwind.config.js
@@ -9,10 +9,13 @@ module.exports = {
         'gray1': '#3C3C3C',
         'orange1': '#F29034',
         'orange2': '#D76E0D',
+        'orange-hover': '#BB6413',
+        'white-hover': '#E1E1E1',
         'green1': '#005E6A',
         'green2': '#074952',
       },
       fontSize: {
+        'body': '1.2rem',
         '2xl': '1.75rem',
         '4xl': '2.875rem',
       }
@@ -22,7 +25,8 @@ module.exports = {
       sm: "640px",
       md: "768px",
       lg: "1024px",
-      xl: "1280px",
+      xl: "1140px",
+      xxl: "1367px",
     },
   },
   plugins: [
