8d35367c6c2827bc3dccb77d3d69a529611cee3e
ragilhadianto <tripleone92@gmail.com>
2024-03-03 21:28:48 +0700

    update

--- WBJ_Sensus_Musik/tailwind.config.js
+++ WBJ_Sensus_Musik/tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
     extend: {
       colors: {
         yellow1: "#F7EF65",
+        pink1: "#F1678B",
+        pink2: "#F3ADB4",
+        pink3: "#CE345B",
+        orange1: "#E85F24",
+        blue1: "#4B96BE",
+        green1: "#97BA5E",
       },
     },
     screens: {
